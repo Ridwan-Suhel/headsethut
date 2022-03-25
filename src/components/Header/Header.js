d7dd696ecaca126eb727766539f2acee464cce42
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-03-25 22:07:23 +0600

    header addede

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h2 className="logo">
+        <a href="/">HeadsetHut.</a>
+      </h2>
+      <p className="tag-line">Pick Your Favourite One.</p>
+    </header>
+  );
+};
+
+export default Header;
